0b993ec8875250b3d941808a7a888753f73c1f5a
Marlo Major <MarloMajor@yahoo.com>
2015-09-14 17:36:17 -0600

    Enigma Project_2

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,17 +1,37 @@
-require "../lib/encrypt_file"
-
 class Enigma
+attr_accessor :key_file, :date_file  # => nil
+
   def initialize(encrypt)
-    @encrypt = encrypt
-  end
+    @encrypt = encrypt     # => ""
+  end                      # => :initialize
 
-end
+  def to_message
+  end             # => :to_message
 
-encrypted = ARGV[0]
-message   = ARGV[1]
+  def key_file
+
+  end  # => :key_file
+
+  def date_file
+    time= Time.new                     # => 2015-09-14 17:30:49 -0600
+    time_string = time.to_s            # => "2015-09-14 17:30:49 -0600"
+    final_string = ""                  # => ""
+    final_string << time_string[8..9]  # => "14"
+    final_string << time_string[5..6]  # => "1409"
+    final_string << time_string[2..3]  # => "140915"
+    final_string                       # => "140915"
+  end                                  # => :date_file
+
+end  # => :date_file
+
+new_enigma = Enigma.new("")  # => #<Enigma:0x007fcba38cdba8 @encrypt="">
+new_enigma.date_file         # => "140915"
+
+message_file = ARGV[0]  # => nil
+encrypt_file = ARGV[1]  # => nil
 
 #Read in the encrypted message
-encrypt = File.read(encrypt_file)
+encrypt = File.read(encrypt_file)  # ~> NoMethodError: undefined method `txt' for nil:NilClass
 
 #Convert encryption to message
 message = Enigma.new(encrypt).to_message
@@ -20,4 +40,9 @@ message = Enigma.new(encrypt).to_message
 File.write(message_file, message)
 
 #Print this summary
-puts "Created #{encrypted.txt} with the key #{key_file} and date #{date_file}"
+puts "Created #{encrypt_file} with the key #{new_enigma.key_file} and date #{new_enigma.date_file}"
+
+# ~> NoMethodError
+# ~> undefined method `txt' for nil:NilClass
+# ~>
+# ~> /Users/marlomajor/code/enigma/lib/encrypt.rb:34:in `<main>'
