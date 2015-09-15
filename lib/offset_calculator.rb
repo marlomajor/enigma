c9928f923a3bd7c10ce2120a6f500fba35b04c0e
Marlo Major <MarloMajor@yahoo.com>
2015-09-14 20:49:55 -0600

    Added functionality for offset calculator

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -3,13 +3,19 @@ require 'pry'           # => true
 class OffsetCalculator
 
   def initialize
-    get_key               = KeyGenerator.new  # ~> NameError: uninitialized constant OffsetCalculator::KeyGenerator
-    new_key               = get_key
+    get_key = KeyGenerator.new  # ~> NameError: uninitialized constant OffsetCalculator::KeyGenerator
+    new_key = get_key
   end                           # => :initialize
 
   def generate_key_offsets(new_key)
-    
-  end                                # => :generate_key_offsets
+    get_keys = ""
+    key_work = new_key.to_s
+    get_keys << key_work[0..1]
+    get_keys << key_work[1..2]
+    get_keys << key_work[2..3]
+    get_keys << key_work[3..4]
+    get_keys
+  end                        # => :generate_key_offsets
 
   def get_date
     time= Time.new
@@ -18,9 +24,18 @@ class OffsetCalculator
     final_string << time_string[8..9]
     final_string << time_string[5..6]
     final_string << time_string[2..3]
-    final_string
-  end           # => :get_date
+    final_string.to_i
+  end
 
+  def rotate_date
+    exponent = get_date ** 2
+    date_array_build(exponent)
+  end
+
+  def date_array_build(exponent)
+    rotate_numbers         = exponent.to_s
+    rotate_date_numbers    = rotate_numbers[-4..-1]        # => :get_date
+  end
 end  # => :get_date
 
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,30 +5,31 @@ require 'pry'
 class EnigmaTest <Minitest::Test
 
   def test_it_exists
-    # skip
     assert Enigma
   end
 
   def test_it_generates_key
-    # skip
     key_new_one = KeyGenerator.new.key_file
     key_new_two = KeyGenerator.new.key_file
     refute_equal key_new_one, key_new_two
   end
 
   def test_we_get_offset_values
-    # skip
     offset_value = OffsetCalculator.new
-    date_value   =
-    offset_value.generate_key_offsets(12345)
+    assert_equal "12233445", offset_value.generate_key_offsets(12345)
   end
 
   def test_we_get_the_correct_date
-    # skip
     date = OffsetCalculator.new
-    assert_equal "140915", date.get_date
+    assert_equal 140915, date.get_date
   end
 
-  
+  def test_rotate_date_works
+    date = OffsetCalculator.new
+    date.rotate_date
+    assert_equal "7225", date.rotate_date
+  end
+
+
 
 end
